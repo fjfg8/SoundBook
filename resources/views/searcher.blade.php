2c36f65251182692cb6ac6efaca1715437be2a84
larg1ua <larg1@alu.ua.es>
2017-05-22 22:34:25 +0200

    Diseño de busqueda terminado

--- resources/views/searcher.blade.php
+++ resources/views/searcher.blade.php
@@ -15,7 +15,7 @@
                 <div class="row" align="center">
                     <form id="buscador" method="POST" action="{{action('SearchController@search')}}">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}"></input>
-                        <div class="input-group" style="width: 50%">
+                        <div class="input-group" style="width: 70%">
                             <input type="text" name="busqueda" class="form-control" placeholder="Introduzca la búsqueda"></input>
                             <div class="input-group-btn">
                                 <select class="form-control select2" style="width: 300%" name="filtro" >
@@ -37,79 +37,98 @@
                         </div>
                     </form>
                 </div>
+                
                 </br>
-            
-                <div class="row" align="center">
-                    <div class="col-md-8">
-                        <div class"box box-primary">
-                            @if(sizeof($busqueda)==0)
-                                <div class="alert alert-info">
-                                    <strong>No se ha encontrado nada</strong>
-                                </div>
-                            @endif
-                            @for($i=0;$i<sizeof($busqueda);$i++)
-                                @if($filtro=="Grupo")
-                                    <div class="box-body" style="background-color:#ffe4c4;">
-                                        <div class="col-md-4" align="center">
-                                            <img class="img-circle" width="50" height="50" src="http://icon-icons.com/icons2/67/PNG/512/group_users_13234.png">
-                                            <h3>{{$busqueda[$i]->name}} </h3>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <p>{{$busqueda[$i]->description}}</p></br>
-                                            <label>{{$type[$i]->type}}</label>
-                                            <a href="/groups/{{$busqueda[$i]->id}}" class="btn btn-primary pull-right" style="padding-top: 5px;">Ver más</a>
-                                        </div>
-                                        
-                                    </div>
-                                @endif
-                                @if($filtro=="Cancion" || $filtro=="Album")
-                                    <div class="box-body" style="background-color:#ffe4c4;">
-                                        <div class="col-md-2" align="center">
-                                            <img src="{{$users[$i]->image}}" width="100" height="100"></img>
-                                            <h3>{{$users[$i]->nick}}</h3>
-                                        </div>
-                                        <div class="col-md-8">
-                                            <h2 class="box-title with-border">{{$busqueda[$i]->title}}</h2>
-                                            <label>Artista: {{$busqueda[$i]->artist}} </label><br/>
-                                            <label>Album: {{$busqueda[$i]->album}}</label></br>
-                                            <label>Genero: {{$type[$i]->type}}</label>
-                                        </div>
-                                        <a href="/song/{{$busqueda[$i]->id}}" class="btn btn-primary pull-right" style="padding-top: 5px;">Ver más</a>
-                                    </div>
-                                @endif
-                                @if($filtro=="Usuario")
-                                    @if($busqueda[$i]->id != Auth::user()->id)
-                                        <div class="box-body" style="background-color:#ffe4c4;">
-                                            <div class="col-md-2" align="center">
-                                                <img src="{{$busqueda[$i]->image}}" width="100" height="100"></img>
-                                                <h3>{{$busqueda[$i]->nick}} </h3>
-                                            </div>
-                                            <div class="col-md-8">
-                                                @if($followers[$i]==1)
-                                                    <form method="POST" action="{{action('UsersController@unfollow')}}">
-                                                    <input type="hidden" name="_method" value="DELETE"></input>
-                                                    <input type="hidden" name="_token" value="{{ csrf_token() }}"></input>
-                                                    <input type="hidden" name="user" value="{{ $busqueda[$i]->id }}"></input>
-                                                        <button class="btn btn-primary pull-right" type="submit">Unfollow</button>
-                                                    </form>
-                                                @endif
-                                                @if($followers[$i]==0)
-                                                    <form method="POST" action="{{action('UsersController@follow')}}">
-                                                    <input type="hidden" name="_method" value="PUT"></input>
-                                                    <input type="hidden" name="_token" value="{{ csrf_token() }}"></input>
-                                                    <input type="hidden" name="user" value="{{ $busqueda[$i]->id }}"></input>
-                                                        <button class="btn btn-primary pull-right" type="submit">Follow</button>
-                                                    </form>
-                                                @endif
-                                                <a href="/visit/{{$busqueda[$i]->id}}" class="btn btn-primary pull-right" style="padding-top: 5px;">Ver más</a>
-                                            </div>  
-                                        </div>
-                                    @endif
-                                @endif
-                            @endfor
-                        </div>
+
+                @if(sizeof($busqueda)==0 || (sizeof($busqueda)==1 && $filtro=="Usuario" && $busqueda[0]->id == Auth::user()->id))
+                    <div class="alert alert-info">
+                        <strong>No se ha encontrado nada</strong>
                     </div>
-                </div>
+                @endif
+                
+                @for($i=0;$i<sizeof($busqueda);$i++)
+                    @if($filtro == "Grupo")
+                        <div class="box box-primary" >
+                            <div class="box-header with-border" style="background: #f4fcff;">
+                                <div class="user-block">
+                                    <img class="img-circle" src="http://icon-icons.com/icons2/67/PNG/512/group_users_13234.png" alt="Group Image">
+                                    <span class="username" color="blue"><a href="/groups/{{$busqueda[$i]->id}}">{{$busqueda[$i]->name}}</a></span>
+                                </div>
+                            </div>
+                            <div class="box-body" style="background: #f4fcff;">
+                                <label>Género:</label> 
+                                <text>{{$type[$i]->type}}</text>
+                                </br>
+                                </br>
+                                <text>{{$busqueda[$i]->description}}</text>
+                            </div>
+                            <div class="box-footer" style="background: #f4fcff;">
+                                <a href="/groups/{{$busqueda[$i]->id}}" class="btn btn-primary pull-right" style="padding-top: 5px;">Ver más</a>
+                            </div>
+                        </div>
+                    @endif
+                    @if($filtro=="Cancion" || $filtro=="Album")
+                        <div class="box box-primary">
+                            <div class="box-header with-border" style="background: #f4fcff;">
+                                <span class="text-muted pull-right">{{$busqueda[$i]->created_at}}</span>
+                                <div class="user-block">
+                                    <img class="img-circle" src="{{$users[$i]->image}}" alt="User Image">
+                                    @if(Auth::user()->id == $users[$i]->id)
+                                        <span class="description"><a href="/home">{{$users[$i]->name}}</a></span>
+                                    @else
+                                        <span class="description"><a href="visit/{{$users[$i]->id}}">{{$users[$i]->name}}</a></span>
+                                    @endif
+                                    <span class="username">{{$busqueda[$i]->title}}</span>
+                                </div>
+                            </div>
+                            <div class="box-body" style="background: #f4fcff;">
+                                <div class="col-md-6" align="center">
+                                    <div class="embed-responsive embed-responsive-16by9">
+                                        <iframe class="embed-responsive-item" src="{{$busqueda[$i]->url}}" allowfullscreen></iframe><br/>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <label>Título: {{$busqueda[$i]->title}} </label><br/>
+                                    <label>Artista: {{$busqueda[$i]->artist}} </label><br/>
+                                    <label>Género: {{$type[$i]->type}}</label>
+                                </div>
+                            </div>
+                            <div class="box-footer" style="background: #f4fcff;">
+                                <a href="/song/{{$busqueda[$i]->id}}" class="btn btn-primary pull-right" style="padding-top: 5px;">Ver más</a>
+                            </div>
+                        </div>
+                    @endif
+                    @if($filtro=="Usuario")
+                        @if($busqueda[$i]->id != Auth::user()->id)
+                            <div class="box box-primary">
+                                <div class="box-header with-border" style="background: #f4fcff;">
+                                    <div class="user-block">
+                                        <img class="img-circle" src="http://www.icon2s.com/img128/128x128-black-white-android-user.png" alt="User Image">
+                                        <span class="username" color="blue"><a href="/visit/{{$busqueda[$i]->id}}">{{$busqueda[$i]->name}}</a></span>
+                                    </div>
+                                </div>
+                                <div class="box-body" style="background: #f4fcff;">
+                                    <div class="col-md-6">
+                                        <img src="{{$busqueda[$i]->image}}" width="200" height="200"></img>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label>Nombre: </label>
+                                        <text> {{$busqueda[$i]->name}}</text></br>
+                                        <label>Nick: </label>
+                                        <text> {{$busqueda[$i]->nick}}</text></br>
+                                        <label>Género: </label>
+                                        <text>{{$busqueda[$i]->gender}}</text></br>
+                                        <label>Preferencias: </label>
+                                        <text> {{$busqueda[$i]->preferences}}</text>
+                                    </div>
+                                </div>
+                                <div class="box-footer" style="background: #f4fcff;">
+                                    <a href="/visit/{{$busqueda[$i]->id}}" class="btn btn-primary pull-right">Ver más</a>
+                                </div>
+                            </div> 
+                        @endif
+                    @endif
+                @endfor
             </div>
         </div>
     </div>
