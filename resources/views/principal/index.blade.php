0b2927321a8d05efcff3ee15f134733ee76b8030
jonay <jgl58@alu.ua.es>
2017-05-22 22:43:41 +0200

    Acabada pagina principalç

--- resources/views/principal/index.blade.php
+++ resources/views/principal/index.blade.php
@@ -24,74 +24,49 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12 text-center">
-					<h1>A few features that should&nbsp;make you excited</h1>
+					
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-md-3 col-sm-6">
 					<div class="feature feature-icon-large">
 						<i class="icon icon-mobile"></i>
-						<h5>Works on all devices</h5>
+						<h5>Comparte musica desde cualquier lado</h5>
 						<p>
-							Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.
+							SoundBook esta disponible en version PC, Android e IOS.
 						</p>
 					</div>
 				</div>
 				<div class="col-md-3 col-sm-6">
 					<div class="feature feature-icon-large">
 						<i class="icon icon-happy"></i>
-						<h5>Friendly interface</h5>
+						<h5>Haz que hablen de tí</h5>
 						<p>
-							Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.
+							Gracias a SoundBook podrás darte a conocer en todos lados.
 						</p>
 					</div>
 				</div>
 				<div class="col-md-3 col-sm-6">
 					<div class="feature feature-icon-large">
 						<i class="icon icon-tools-2"></i>
-						<h5>24/h support</h5>
+						<h5>Soporte 24/h</h5>
 						<p>
-							Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.
+							Soporte 24h. En caso de algún fallo, contacta con nosotros y lo solucionamos enseguida.
 						</p>
 					</div>
 				</div>
 				<div class="col-md-3 col-sm-6">
 					<div class="feature feature-icon-large">
-						<i class="icon icon-lock"></i>
-						<h5>User protection</h5>
+						<i class="icon icon-search"></i>
+						<h5>Comparte</h5>
 						<p>
-							Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.
+							Busca y comparte contenidos con otras personas con nuestro servicio de subida de archivos. 
 						</p>
 					</div>
 				</div>
 			</div>
 		</div>
 	
-	</section>
-	
-	<section class="bg-primary">
-		<div class="container">
-			<div class="row">
-				<div class="col-sm-8 col-sm-offset-2">
-					<div class="testimonials-slider text-center">
-						<ul class="slides">
-							<li>
-								<p class="text-white lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-								<span class="author text-white">John Doe - Graphic Designer</span>
-							</li>
-							<li>
-								<p class="text-white lead">Unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae sunt explicabo ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugi.</p>
-								<span class="author text-white">Sarah Brown - UX Designer</span>
-							</li>
-							<li>
-								<p class="text-white lead">Ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.</p>
-								<span class="author text-white">Richard Hanson - Web developer</span>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
 	</section>
 </div>
 				
